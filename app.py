a74bcdaa127d4c56dc89b85bc96d97583f1844ed
mahdi10000 <92900618+mahdi10000@users.noreply.github.com>
2026-06-20 01:17:18 +0330

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messenger.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 # ===== مدل‌ها =====
 
